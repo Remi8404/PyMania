a4a8cace5ac94c98c05f1209719cd1cd6f6a4c99
remia <remi.aerts04@gmail.com>
2025-11-15 18:42:22 +0100

    using json to stock config

--- src/config/config_handler.py
+++ src/config/config_handler.py
@@ -1,15 +1,58 @@
+import json
+import os
 
-from Config.default_config import config as defaultConfig
-from Config.local_config import config as localConfig
+DEFAULT_FILE = 'default_config.json'
+LOCAL_FILE = 'local_config.json'
 
-def configHandler() :
-    config: dict[str, str] = {}
-    defaultKeys: set[str] = defaultConfig.keys()
-    localKeys: set[str]  = localConfig.keys()
-    for key in defaultKeys : 
-        if key in localKeys :
-            config[key] = localConfig[key]
-        else :
-            config[key] = defaultConfig[key]
+script_dir = os.path.dirname(os.path.abspath(__file__))
+
+default_path = os.path.join(script_dir, DEFAULT_FILE)
+local_path = os.path.join(script_dir, LOCAL_FILE)
+
+def configHandler() -> dict[str, str]:
+    try:
+        with open(default_path, 'r') as f:
+            default_config = json.load(f)
+    except FileNotFoundError:
+        print(f"Error : File '{DEFAULT_FILE}' not found.")
+        return {}
+    except json.JSONDecodeError:
+        print(f"Error: File '{DEFAULT_FILE}' is not a valid JSON.")
+        return {}
+
+    config = default_config.copy() 
+    
+    if os.path.exists(local_path):
+        try:
+            with open(local_path, 'r') as f:
+                local_config = json.load(f)
+            config.update(local_config)
+            print(f"Configuation set to '{LOCAL_FILE}'.")
+            
+        except json.JSONDecodeError:
+            print(f"Warning : '{LOCAL_FILE}' corrupted or empty. Using default values.")
+
+    else:
+        print(f"Error : File '{LOCAL_FILE}' not found. Default values used instead")
+            
     return config
-     
+
+
+def set_config_value(key: str, value):
+    if os.path.exists(local_path):
+        try:
+            with open(local_path, 'r') as f:
+                local_config = json.load(f)
+        except (FileNotFoundError, json.JSONDecodeError):
+            local_config = {}
+    else:
+        local_config = {}
+    local_config[key] = value
+    try:
+        with open(local_path, 'w') as f:
+            json.dump(local_config, f, indent=4) 
+        print(f"\nValue of '{key}' updated in {LOCAL_FILE}.")
+    except Exception as e:
+        print(f"\nError while writing in {LOCAL_FILE}: {e}")
+
+
--- src/config/default_config.py
+++
@@ -1,3 +0,0 @@
-config: dict[str, str] = {
-    "openplanetPath": "",
-}
